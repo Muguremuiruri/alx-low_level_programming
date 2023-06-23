07c87853896853e69e0717d73ec2a9a4b83cf8e2
muguremuiruri <muirurivictoria68@gmail.com>
2023-06-23 01:35:43 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+/**
+ * _isdigit - Checks if a character is a digit.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if @c is a digit, 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);  /* c is a digit */
+	}
+	return (0);  /* c is not a digit */
+}
+
